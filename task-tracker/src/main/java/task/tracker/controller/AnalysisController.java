11d0ffe85cc09da0ed39c4a83fb2e9eb3863216b
Nick Palladino <np398@cornell.edu>
2020-07-04 16:12:59 -0400

    Added endpoint

--- task-tracker/src/main/java/task/tracker/controller/AnalysisController.java
+++ task-tracker/src/main/java/task/tracker/controller/AnalysisController.java
@@ -1,5 +1,25 @@
 package task.tracker.controller;
 
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import task.tracker.service.AnalysisService;
+
+import javax.inject.Inject;
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Set;
+
+@Controller("/v1")
 public class AnalysisController {
+
+    @Inject
+    AnalysisService analysisService;
+
+    @Post("/analysis")
+    @Produces(MediaType.APPLICATION_JSON)
+    public HttpResponse<Map<String, BigDecimal>> analysis(@Body Set<String> taskNames) {
+        return HttpResponse.ok(analysisService.analyze(taskNames));
+    }
 
 }
--- task-tracker/src/main/java/task/tracker/service/AnalysisService.java
+++ task-tracker/src/main/java/task/tracker/service/AnalysisService.java
@@ -1,13 +1,34 @@
 package task.tracker.service;
 
+import lombok.NonNull;
+import task.tracker.client.ActivityWatcherClient;
 import task.tracker.client.model.Event;
 
+import javax.inject.Singleton;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 
 import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
 
+@Singleton
 public class AnalysisService {
+
+    private ActivityWatcherClient client;
+
+
+    public AnalysisService() throws MalformedURLException {
+        client = new ActivityWatcherClient(new URL("http://localhost:5600"));
+    }
+
+
+    public Map<String, BigDecimal> analyze(@NonNull Set<String> taskNames) {
+        List<Event> events = client.getWindowActiveNotAfkEventsSortedByTimestamp();
+        return calculateTaskTimes(events, taskNames);
+    }
+
 
     public Map<String, BigDecimal> calculateTaskTimes(List<Event> events, Set<String> taskNames) {
 
@@ -15,15 +36,19 @@ public class AnalysisService {
         String currentTask = "INITIAL";
 
         for (Event event : events) {
-
             for (String taskName : taskNames) {
                 if (containsIgnoreCase(event.getData().getTitle(), taskName)) {
                     currentTask = taskName;
+                    break;
                 }
             }
 
             taskTimes.merge(currentTask, event.getDuration(), BigDecimal::add);
         }
+
+        // convert to hours from seconds with 1 decimal place and rounding
+        taskTimes.replaceAll((k, v) -> v.divide(BigDecimal.valueOf(3600), 1, RoundingMode.HALF_UP));
+
         return taskTimes;
     }
 
